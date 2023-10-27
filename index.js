7a6bcda4ba9d96083006bbee7b222c52044d2227
asonyue <410856065@gms.tku.edu.tw>
2023-10-27 11:28:57 -0400

    Adding Full functionality

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ let itemList = [];
 let itemList2 = [];
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended : true}));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    const category = 'personal';
-    res.render("index.ejs", { items: itemList , category: category});
+    const category = "personal";
+    res.render("index.ejs", { items: itemList, category: category });
 });
 
 app.get("/work", (req, res) => {
-    const category = 'work';
-    res.render("index.ejs", { items: itemList2 , category: category}) ;
+    const category = "work";
+    res.render("index.ejs", { items: itemList2, category: category });
 });
 
 app.post("/", (req, res) => {
     const itemToAdd = req.body.add;
     if (itemToAdd) {
-        itemList.push(itemToAdd);
+        itemList.push({ text: itemToAdd, done: false });
     }
     res.redirect("/");
 });
@@ -31,9 +32,20 @@ app.post("/", (req, res) => {
 app.post("/work", (req, res) => {
     const itemToAdd = req.body.add;
     if (itemToAdd) {
-        itemList2.push(itemToAdd);
+        itemList2.push({ text: itemToAdd, done: false });
     }
     res.redirect("/work");
+});
+
+app.post('/mark-done', (req, res) => {
+    const taskIndex = parseInt(req.body.taskIndex, 10); // Parse the task index from the form data
+    const targetList = req.body.category === 'work' ? itemList2 : itemList;
+
+    if (taskIndex >= 0 && taskIndex < targetList.length) {
+        targetList[taskIndex].done = true; // Mark the task as done
+    }
+
+    res.redirect(req.body.category === 'work' ? '/work' : '/');
 });
 
 app.listen(port, () => {
